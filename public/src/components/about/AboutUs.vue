1b50115cc81b6474b381af10912b97471442408a
Redoni18 <redonemini18@gmail.com>
2022-07-27 19:34:19 +0200

    added maps on about-us page

--- public/src/components/about/AboutUs.vue
+++ public/src/components/about/AboutUs.vue
@@ -9,17 +9,63 @@
                 <br>
 The World Bank eLibrary is a subscription-based website for institutions and has been serving the unique needs of researchers and librarians since 2003. It is dedicated to offering researchers quick and easy access to the complete collection of World Bank formal publications and journals since the 1990s, as well as reports, working papers, and top data from the World Bank’s World Development Indicators – all in one user-friendly platform. Content in the eLibrary is carefully written and curated to meet the highest academic and editorial standards. eLibrary users are assured full and immediate access to all academic research and scholarship published by the World Bank – the majority of which is made available through a Creative Commons Attribution (CC BY) license.
                 </p>
+                <router-link type="submit" class="btn btn-primary contact-button" :to="{path: '/contact-us'}">Get in touch</router-link>
             </div>
             <div class="about-us__image">
                 <img src="../../assets/8364.jpg" alt="" style="width: 500px;">
             </div>
-        </div>
 
-        <div class="mb-3">
-            <router-link type="submit" class="btn btn-primary" :to="{path: '/contact-us'}">Get in touch</router-link>
-        </div> 
+
+        </div>
+        <l-map style="height: 500px;width:1000px;margin-top:15px;" :zoom="zoom" :center="center">
+            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+            <l-marker v-for="(marker,index) in markerLatLng" :key="index" :lat-lng="marker"></l-marker>
+        </l-map>
     </div>
 </template>
+
+<script>
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import axios from 'axios'
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+        latlng: [],
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution:
+            '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        zoom: 5,
+        center: [51.505, -0.159],
+        markerLatLng: []
+    };
+  },
+  async mounted() {
+    await this.fetchLocations()
+  },
+  methods: {
+    async fetchLocations(){
+        const response = await axios.get("http://localhost:8000/api/locations")
+        this.locations = response.data
+        for(let i = 0;i<this.locations.length;i++){
+            this.latlng = []
+            if(this.locations[i].latitude && this.locations[i].longitude){
+
+                this.latlng.push(this.locations[i].latitude)
+                this.latlng.push(this.locations[i].longitude)
+                this.markerLatLng.push(this.latlng)
+                this.center = this.markerLatLng[0]
+            }
+        }
+    },
+  }
+}
+</script>
+
 
 <style scoped>
 
@@ -40,5 +86,16 @@ The World Bank eLibrary is a subscription-based website for institutions and has
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.about-us__content{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.about-us__content .contact-button{
+    margin: auto;
+    width: 40%;
 }
 </style>
--- public/src/main.js
+++ public/src/main.js
@@ -22,7 +22,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vSelect from 'vue-select'
 
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
 Vue.component('v-select', vSelect) 
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
